f07b71ed5c0e155180a9d6b272590488699e1ac8
portal martin <ptl_martin@yahoo.co.jp>
2024-11-03 16:40:46 +0900

    implement contact in about section, and resume

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,8 +24,8 @@ const Hero = () => {
           className="rounded-full"
           src="/profile_img.jpeg"
           alt="Profile picture"
-          width={150} // Adjusted size for mobile
-          height={150}
+          width={200} // Adjusted size for mobile
+          height={200}
         />
         <h3 className="text-lg md:text-xl font-bold mt-4">PORTAL MARTIN</h3>
         <p className="text-gray-600 text-sm md:text-base">
