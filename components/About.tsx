8462f28beed2db7b15547e907c65a7615133981e
portal martin <ptl_martin@yahoo.co.jp>
2025-02-16 17:14:13 +0900

    Prepare app for Heroku deployment

--- components/About.tsx
+++ components/About.tsx
@@ -1,20 +1,4 @@
 import ContactSection from "./ContactSection";
-import { Button } from "./ui/button";
-
-const ResumeButton = ({
-  onClick,
-  children,
-}: {
-  onClick: () => void;
-  children: React.ReactNode;
-}) => (
-  <Button
-    onClick={onClick}
-    className="mt-6 px-6 py-3 bg-primary text-white font-semibold rounded-lg hover:bg-primary/70 transition duration-200"
-  >
-    {children}
-  </Button>
-);
 
 const About = () => {
   return (
