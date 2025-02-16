82c7bc6b2b3bc56b52556a49dcda9809a75d1c0e
portal martin <ptl_martin@yahoo.co.jp>
2025-02-16 17:08:45 +0900

    Prepare app for Heroku deployment

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,16 +8,16 @@ const Contact = () => {
   return (
     <div className="flex mt-3 gap-4">
       <Link href="https://github.com/PtlMarty" target="_blank">
-        <Github className="hover:scale-110 transition-transform transition-colors duration-300 hover:text-[#3ac58f] " />
+        <Github className="hover:scale-110 transition duration-300 hover:text-[#3ac58f]" />
       </Link>
       <Link
         href="https://www.linkedin.com/in/martin-portal-292b12289/"
         target="_blank"
       >
-        <Linkedin className="hover:scale-110 transition-transform transition-colors duration-300 hover:text-[#3ac58f] " />
+        <Linkedin className="hover:scale-110 transition duration-300 hover:text-[#3ac58f]" />
       </Link>
       <Link href="mailto:ptl_martin@yahoo.co.jp">
-        <LucideMail className="hover:scale-110 transition-transform transition-colors duration-300 hover:text-[#3ac58f] " />
+        <LucideMail className="hover:scale-110 transition duration-300 hover:text-[#3ac58f]" />
       </Link>
     </div>
   );
